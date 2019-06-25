d40b238cbde570bfe2d3d20eebae4b84746d6983
Katherine Love <katherinelove@Katherines-iMac.local>
2019-06-25 23:26:56 +1200

    Hotel button changing

--- summative/js/script.js
+++ summative/js/script.js
@@ -9,3 +9,8 @@ function banner() {
     document.getElementById('banner').classList.remove('upwards');
     document.getElementById('accommodation').classList.remove('moving');
 }
+
+function changeColour() {
+  console.log('Button has changed colour');
+  document.getElementsByClassName('hotel')[0].style.backgroundColor = 'black';
+}
